b74396cea3821dfcf5cb8c7f827454903b3e580e
Wal Monfortt <jose.gual.monfortte.f@gmail.com>
2024-12-29 11:16:56 -0600

    update project
    fix inner circle
    remove useless sound
    add flash function
    add makesound function

--- index.js
+++ index.js
@@ -1,15 +1,43 @@
 var score = 0,
     display = document.getElementById('num');
 
-var plus = document.getElementById('plus')
+let buttonColours = ["green", "red", "yellow", "blue", "sea-blue", "black", "brown", "gray" ]
+
+let gamePattern = [];
+let userClickedPattern = [];
+
+let currentLevel = 0;
+
+
+
+let speed = 0.5
+
+$(document).keydown(function (e) {
+  console.log(e.key)
+  if(e.key === 'a'){
+    startGame()
+  }
+})
+
+// var plus = document.getElementById('plus')
 
 display.innerText = score;
 
-plus.addEventListener('click', function(){
-  score++;
-  display.innerText = score;
-  enabled();
-});
+// plus.addEventListener('click', function(){
+//   score++;
+//   display.innerText = score;
+//   console.log(`random ${nexSequence()}`)
+//   enabled();
+// });
+
+let buttons = $('.btn')
+buttons.on('click', function(){
+  const btn = this.id
+  flashBtn(btn)
+  makeSound(btn)
+  userClickedPattern.push(btn)
+  console.log(userClickedPattern)
+})
 
 
 function reset() {
@@ -17,3 +45,35 @@ function reset() {
     display.innerText = score;
 };
 
+// Game functions
+
+
+function nextElement() {
+  let max = 4;
+  if (currentLevel > 5) {
+    max = 8;
+  }
+  return Math.floor(Math.random() * max )
+}
+
+
+function flashBtn(btn) {
+  $(`#${btn}`).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+}
+
+function makeSound(file) {
+  var audio = new Audio(`./sounds/${file}.mp3`);
+audio.play();
+}
+
+function startGame() {
+  const randomChosenColour = nextElement()
+  const nextColor = buttonColours[randomChosenColour]
+  gamePattern.push(nextColor)
+
+  makeSound(nextColor)
+  flashBtn(nextColor)
+
+  console.log(`game Pattern ${gamePattern}`)
+}
+// makeSound(buttonColours[randomChosenColour])
